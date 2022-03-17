f7fc42367385b987633e52e24ec34c595cb0948c
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-03-16 22:35:15 -0300

    Adicionar arquivos de projeto.

---
+++ Curso02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        } 
+    }
+}
