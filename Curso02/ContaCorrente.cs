0a892ce5bee60980f954334aa041b85eb3091290
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-03-17 23:14:08 -0300

    Criação das classes ContaCorrente e Cliente, com suas propriedades.

---
+++ Curso02/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso02
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int agencia, int numeroConta)
+        {
+            Agencia = agencia;
+            NumeroConta = numeroConta;
+        }
+
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+    }
+}
