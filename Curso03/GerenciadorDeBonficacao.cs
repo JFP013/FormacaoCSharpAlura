12520013cf3e585f559e6a14343d061fe5850fb2
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-03-21 20:42:48 -0300

    Commit Parcial curso 03. Criada as classes Funcionario, funcionario, diretor e Gerenciador de Bonificação.

---
+++ Curso03/GerenciadorDeBonficacao.cs
@@ -0,0 +1,24 @@
+﻿using Curso03.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso03
+{
+    public class GerenciadorDeBonficacao
+    {
+        public double TotalBonificacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            TotalBonificacao += funcionario.GetBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return TotalBonificacao;
+        }
+    }
+}
---
+++ Curso03/Program.cs
@@ -0,0 +1,37 @@
+﻿using Curso03.Funcionarios;
+using System;
+
+namespace Curso03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorDeBonficacao gerenciadorDeBonificacao = new GerenciadorDeBonficacao();
+
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Jefferson";
+            funcionario.Salario = 2000;
+
+            gerenciadorDeBonificacao.Registrar(funcionario);
+
+            Diretor diretor = new Diretor();
+            diretor.Nome = "Camila";
+            diretor.Salario = 5000;
+
+            gerenciadorDeBonificacao.Registrar(diretor);
+
+            Console.WriteLine("O salario do " + funcionario.Nome + " é: " + funcionario.Salario);
+            Console.WriteLine("A bonificação do " + funcionario.Nome + " é de: " + funcionario.GetBonificacao());
+
+            Console.WriteLine("O salario do " + diretor.Nome + " é: " + diretor.Salario);
+            Console.WriteLine("A bonificação do " + diretor.Nome + " é de: " + diretor.GetBonificacao());
+
+            Console.WriteLine("O total de bonificação é de: " + gerenciadorDeBonificacao.GetTotalBonificacao());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
