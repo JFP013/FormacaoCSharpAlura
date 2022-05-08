200efb6aff55fe912361211b6b35e8e53986730e
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-05-08 01:03:55 -0300

    Expressões regulares.

--- Curso05/Program.cs
+++ Curso05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Curso06
 {
@@ -6,6 +7,16 @@ namespace Curso06
     {
         static void Main(string[] args)
         {
+            //Expressões regulares
+            string expressao = "[0-9]{5}-?[0-9]{4}";
+            string text = "Meu nome é jefferson e meu tel é 98124-3528";
+
+            Match resultado = Regex.Match(text, expressao);
+
+            Console.WriteLine(resultado.Value);
+
+            Console.ReadLine();
+
             //Entendendo a Substring
             string url = "www.teste.com.br/cambio/?entrada=dolar&saida=real&valor=1500";
 
