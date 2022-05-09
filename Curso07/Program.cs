d176ec14afb8b2f89703a5aa53a2377741bb1f89
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-05-09 00:57:13 -0300

    Criando projeto 07 e criando arrays de ContaCorrente

---
+++ Curso07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Curso07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando array de conta corrente
+
+            ContaCorrente[] ContasCorrente = new ContaCorrente[]
+            {
+                new ContaCorrente(123,123456),
+                new ContaCorrente(123,123654),
+                new ContaCorrente(123,456321)
+            };
+            
+            for (int i = 0; i < ContasCorrente.Length; i++)
+            {
+                Console.WriteLine($"Agencia:{ContasCorrente[i].Agencia} ContaCorrente:{ContasCorrente[i].NumeroConta}"); 
+            }
+
+            Console.WriteLine($"O número de contas criadas é de: {ContasCorrente.Length}");
+            Console.ReadLine();
+        }
+    }
+}
