b50b4b7a958f0230218b00e09d5d6d935770d563
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-05-19 01:00:26 -0300

    Commit criando metodos de extensão.

---
+++ Curso08/ListExtensao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso08
+{
+    public static class ListExtensao
+    {
+        public static void AdcionarVarios(this List<int> listaInteiros, params int[] inteiros)
+        {
+            foreach (int item in inteiros)
+            {
+                listaInteiros.Add(item);
+            }
+        }
+    }
+}
--- Curso08/Program.cs
+++ Curso08/Program.cs
@@ -15,6 +15,8 @@ namespace Curso08
             idades.Add(31);
             idades.Add(61);
 
+            idades.AdcionarVarios(15, 22, 34, 35, 38, 40);
+
             for (int i = 0; i < idades.Count; i++)
             {
                 Console.WriteLine(idades[i]);
