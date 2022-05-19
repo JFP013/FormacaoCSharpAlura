9f4be871ccf1b5325dc2446a6e3e72d095282d3d
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-05-19 00:54:13 -0300

    Commit criando projeto curso08

---
+++ Curso08/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Curso08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+
+            idades.Add(1);
+            idades.Add(12);
+            idades.Add(21);
+            idades.Add(31);
+            idades.Add(61);
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
