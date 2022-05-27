4c10fb0a2d4fdeec8cc28ceffa48456a03b2c1ed
jefferson Ferreira Piedade <jfdapiedade@gmail.com>
2022-05-27 00:05:32 -0300

    classe partial de program

---
+++ Curso09/ManipulandoDiretamenteOFileStream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Curso09
+{
+    partial class Program
+    {
+        static void ManipulandoDiretamenteOFileStream()
+        {
+            string diretorioArquvio = "Contas.txt";
+
+            FileStream fluxoArquivo = new FileStream(diretorioArquvio, FileMode.Open);
+
+            byte[] buffer = new byte[1024]; //1KB
+            int restanteFluxo = -1;
+
+            while (restanteFluxo != 0)
+            {
+                restanteFluxo = fluxoArquivo.Read(buffer, 0, 1024);
+                LerArquivoTexto(buffer);
+            }
+            Console.ReadLine();
+        }
+
+        static void LerArquivoTexto(byte[] buffer)
+        {
+            var utf8 = new UTF8Encoding();
+
+            string texto = utf8.GetString(buffer);
+
+            Console.Write(texto);
+
+            //foreach (byte meuByte in buffer)
+            //{
+            //    Console.WriteLine(meuByte);
+            //}
+        }
+    }
+}
